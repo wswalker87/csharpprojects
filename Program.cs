00af86dd20daa4899a595a2ffe0bbd28dc34fcc9
wswalker87 <scott@wscottwalker.com>
2024-01-22 20:21:20 -0600

    commit message

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ int secondValue = 600;
 int largerValue;
 
 //This is me trying to get this to commit
+//This is me adding a second line to make sure this is good
 largerValue = Math.Max(firstValue, secondValue);
 
 Console.WriteLine(largerValue);
